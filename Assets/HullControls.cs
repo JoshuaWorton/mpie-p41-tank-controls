c63fe2e926f1475ef4946deb130669d5f51511b0
JoshuaWorton <rjr560@york.ac.uk>
2025-10-15 09:40:54 +0100

    Update HullControls.cs

--- Assets/HullControls.cs
+++ Assets/HullControls.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class HullControls : MonoBehaviour
 {
+    public float speed = 50.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,8 +11,16 @@ public class HullControls : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update ()
     {
+        float throttle = Input.GetAxis("Vertical");
+        float movement = throttle * speed * Time.deltaTime;
         
+        Transform t = gameObject.transform;
+        t.Translate (0.0f, 0.0f, movement);
+
+        float steering = Input.GetAxis("Horizontal");
+        float turn = steering * turnSpeed * Time.deltaTime;
+        t.Rotate (0.0f, turn, 0.0f);
     }
 }
